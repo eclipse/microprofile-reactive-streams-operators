6a4c686511bfcb22b7bf499dfe78cde71d00bb71
James Roper <james@jazzy.id.au>
2018-08-15 11:40:46 +1000

    Generate marble diagrams as part of build
    As discussed in the MicroProfile Reactive weekly hangout, I've deleted
    all the marble diagram PNGs from the repo, and instead ensured they are
    built by Maven. This is using the Maven frontend plugin to download and
    install node and npm, and then running the script using npm. The
    diagrams are generated at the last possible minute before they are
    needed, that is, in the prepare-package phase.

--- api/src/docs/js/generate-marble-diagrams.js
+++ api/src/docs/js/generate-marble-diagrams.js
@@ -21,14 +21,15 @@
  * Run this script in Puppeteer (ie, headless Chrome) to actually render each diagram.
  */
 const htmlPage = "file://" + __dirname + "/puppeteer.html";
-const outputDir = __dirname + "/../../main/java/org/eclipse/microprofile/reactive/streams/doc-files";
+const outputDir = __dirname + "/../../../target/apidocs/org/eclipse/microprofile/reactive/streams/doc-files";
+
+const shell = require("shelljs");
+shell.mkdir("-p", outputDir);
 
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({
-    args: ["--disable-gpu"]
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   // Important to wait until networkidle0, otherwise fonts won't be loaded.
   await page.goto(htmlPage, {waitUntil: "networkidle0"});
@@ -38,12 +39,10 @@ const puppeteer = require("puppeteer");
     return Object.keys(marbles.graphs);
   });
 
-  console.log("Rendering diagrams for " + keys);
-
   const renderDiagrams = async (keys) => {
     if (keys.length > 0) {
       const key = keys[0];
-      console.log("Rendering diagram for " + key);
+      console.log("Rendering marble diagram for " + key);
       const dimensions = await page.evaluate((key) => {
         const diagram = window.document.getElementById("diagram");
         diagram.innerHTML = "";
